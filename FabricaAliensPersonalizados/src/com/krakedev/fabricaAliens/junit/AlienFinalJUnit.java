d960db61da64666ff32ebf856dee99bbffe8666f
Draemiz <draemizgx1@gmail.com>
2026-04-23 20:00:46 -0500

    se agrego mas pruebas finales

--- FabricaAliensPersonalizados/src/com/krakedev/fabricaAliens/junit/AlienFinalJUnit.java
+++ FabricaAliensPersonalizados/src/com/krakedev/fabricaAliens/junit/AlienFinalJUnit.java
@@ -19,4 +19,42 @@ public class AlienFinalJUnit {
 
         assertTrue(alien.getPrecioTotal() > 0);
     }
+    
+    @Test
+    public void noPermiteExcesoExtremidades() {
+        Alien alien = new Alien(15, "Verde");
+
+        assertTrue(alien.agregarBrazos(6));
+     // pasa de 10
+        assertFalse(alien.agregarPiernas(5));
+
+        assertEquals(6, alien.getNumeroBrazos());
+        assertEquals(0, alien.getNumeroPies());
+    }
+    
+    @Test
+    public void respetaLimiteOjos() {
+    	// max 3
+        Alien alien = new Alien(8, "Rojo"); 
+
+        assertTrue(alien.agregarOjos(3));
+        
+        //se pasa
+        assertFalse(alien.agregarOjos(1)); 
+
+        assertEquals(3, alien.getNumeroOjos());
+    }
+    
+    @Test
+    public void precioTotalCorrecto() {
+        Alien alien = new Alien(10, "Azul");
+
+        alien.agregarBrazos(2);
+        alien.agregarPiernas(2);
+        alien.agregarOjos(2);
+
+        double esperado = 2.0 + (4 * 1.0) + (2 * 0.5);
+
+        assertEquals(esperado, alien.getPrecioTotal(), 0.001);
+    }
 }
